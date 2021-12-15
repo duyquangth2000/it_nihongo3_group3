53c299759d5380d3eb6c003668d39e82733f1596
duyquangth2000 <79646329+duyquangth2000@users.noreply.github.com>
2021-12-15 22:03:32 +0700

    First commit

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dnmsk7zxb"
+  config.api_key = "285492479139775"
+  config.api_secret = "gc7BSgqA1ShHnDvkpaNy9nwstGU"
+  config.cdn_subdomain = true
+end
